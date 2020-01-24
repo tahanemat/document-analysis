6e7dac3470a5d3b5536a8ccb8dc112cbd539c3c4
Taha Nemat <tahanemat6@gmail.com>
2020-01-24 06:50:33 +0500

    implemented all services and their inter-communications, added ajax on frontend

---
+++ extractor-app/ProdCon/ProdCon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+class Receive
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "extract", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                Console.WriteLine("Receive a message!");
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                // extract dates and amounts using regex
+                channel.BasicPublish(exchange: "", routingKey: "result", basicProperties: null, body:body);
+                Console.WriteLine("Sent a message! " + message);
+            };
+            channel.BasicConsume(queue: "extract", autoAck: true, consumer: consumer);
+            Console.ReadLine();
+        }
+    }
+}
--- flask-app/init.py
+++ flask-app/init.py
@@ -1,11 +1,27 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, Response
 from PIL import Image
 from pdf2image import convert_from_bytes
 import pytesseract
+import pika
 import os
+import time
 app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = ['.pdf','.png']
+
+connected = False
+
+while connected == False:
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        channel = connection.channel()
+        channel.queue_declare(queue='extracted')
+        connected = True
+        print('connection successful')
+    except Exception as e:
+        print('connection failed because ' + str(e))
+        time.sleep(3)
+
 
 @app.route('/')
 def home():
@@ -13,23 +29,36 @@ def home():
 
 @app.route('/submit_file', methods=['POST'])
 def perform_ocr():
-    file_to_extract = request.files['uploadedfile']
+    file_to_extract = request.files['file']
     file_type = os.path.splitext(file_to_extract.filename)[1]
     if file_type not in ALLOWED_EXTENSIONS:
         return 'Invalid file format, only .png and .pdf allowed!'
 
     if file_type == '.png':
         image = Image.open(file_to_extract)
-        print(image)
         extracted_text = pytesseract.image_to_string(image)
-        return extracted_text
+        channel.basic_publish(exchange='', routing_key='extract', body=extracted_text)
+        return ('', 204)
 
     if file_type == '.pdf':
         file_bytes = file_to_extract.read()
         image = convert_from_bytes(file_bytes, fmt="png", transparent=True, single_file=True)
         extracted_text = pytesseract.image_to_string(image[0])
-        return extracted_text
-    
+        channel.basic_publish(exchange='', routing_key='extract', body=extracted_text)
+        return ('', 204)
+
+
+def event_stream(rec_channel):
+    for method, properties, body in rec_channel.consume(queue='result', auto_ack=True):
+        message = body.decode('utf-8')
+        yield 'data: %s\n\n' % message
+
+@app.route('/stream')
+def stream():
+    rec_connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    rec_channel = rec_connection.channel()
+    rec_channel.queue_declare(queue='result')
+    return Response(event_stream(rec_channel),mimetype="text/event-stream")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
